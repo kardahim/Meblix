ebedc415ebd26d3897dd378bf4f911001bdb6b62
Paweł Ciszewski <kardahim@gmail.com>
2021-12-30 17:37:16 +0100

    add login

---
+++ resources/views/users/login.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+@section('content')
+<div class="content-size container">
+    <form action="/login" method="POST">
+        @csrf
+        {{-- email address --}}
+        <div class="form-group offset-4">
+            <label for="exampleInputEmail1" style="font-weight:bold">Adres email</label>
+            <input name="email" type="email" class="form-control col-sm-6" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+        </div>
+        {{-- password --}}
+        <div class="form-group offset-4">
+            <label for="exampleInputPassword1" style="font-weight:bold">Hasło</label>
+            <input name="password" type="password" class="form-control col-sm-6" id="exampleInputPassword1" placeholder="Hasło">
+        </div>
+        {{-- submit button --}}
+        <button type="submit" class="btn btn-primary offset-4">Zaloguj się</button>
+    </form>
+</div>
+@endsection
