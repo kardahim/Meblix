fe1bd054d77b54a8a898cdb9b8be7a2538885869
Paweł Ciszewski <kardahim@gmail.com>
2021-12-30 14:49:16 +0100

    add footer to template

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: #f8f9fa;">
+    &copy;2021
+    <a class="text-dark" href="https://github.com/kardahim">P.Ciszewski</a>
+  </div>
+  <!-- Copyright -->
+</footer>
