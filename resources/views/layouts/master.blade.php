7fba8676f2fc7d5cbea8427ed62c4d023d21a619
Paweł Ciszewski <kardahim@gmail.com>
2021-12-30 14:41:23 +0100

    pages template finish

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Meblix</title>
+    {{-- Bootstrap CSS--}}    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    {{-- Bootstrap JS, jQuery--}}    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    {{-- custom CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{-- custom JS --}}
+    <script src="{{ asset('js/scriprs.js') }}"></script>
+</head>
+<body>
+    {{-- header --}}
+    {{ View::make('layouts.header') }}
+    {{-- content --}}
+    @yield('content')
+    {{-- footer --}}
+    {{ View::make('layouts.footer') }}
+</body>
+</html>
